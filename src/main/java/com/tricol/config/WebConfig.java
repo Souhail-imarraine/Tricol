9cd386c579fb20daed110f924366d03a14cf74f3
imarrainesouhail202@gmail.com <imarrainesouhail202@gmail.com>
2025-10-26 13:33:11 +0100

    ajouter la configuration de web Config

--- src/main/java/com/tricol/config/WebConfig.java
+++ src/main/java/com/tricol/config/WebConfig.java
@@ -1,5 +1,13 @@
 package com.tricol.config;
 
-public class WebConfig {
-    
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.tricol.controller")
+public class WebConfig implements WebMvcConfigurer {
+
 }
