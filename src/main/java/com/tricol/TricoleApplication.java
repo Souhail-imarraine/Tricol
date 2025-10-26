3a1e7886086e34e0453a0be96b086c8ac9764f0c
imarrainesouhail202@gmail.com <imarrainesouhail202@gmail.com>
2025-10-26 13:48:31 +0100

    ajouter le poit d'entree de l'application

--- src/main/java/com/tricol/TricoleApplication.java
+++ src/main/java/com/tricol/TricoleApplication.java
@@ -1,5 +1,12 @@
 package com.tricol;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import com.tricol.config.AppConfig;
+
 public class TricoleApplication {
-    
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        System.out.println("Tricole Application Started!");
+    }
 }
